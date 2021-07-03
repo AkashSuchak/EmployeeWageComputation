12a271ccd2d360bf3953c44c4435c59f353ca678
AkashSuchak <suchak.akash@gmail.com>
2021-07-04 01:08:24 +0530

    Add : Employee Daily Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -6,10 +6,18 @@
 #Random Number Generate
 check=$((RANDOM%2))
 
+#Assign values
+wagePerHr=20
+fullDayHrs=8
+
+#Calculate Daily Wage
+dailyWage=$((wagePerHr * fullDayHrs))
+
 #Check Employee is Present or Absent
 if [ $check -eq 1 ]
 then
 	echo "Employee is Present"
+	echo "Daily Wage : $dailyWage"
 else
 	echo "Employee is Absent"
 fi
