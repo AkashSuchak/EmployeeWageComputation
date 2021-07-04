2805fb220ba15159f549448773de0ff4b8fd0165
AkashSuchak <suchak.akash@gmail.com>
2021-07-04 14:54:26 +0530

    ADD : Employee Monthly Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,7 +1,7 @@
-#! /bin/bash
+#! /bin/bash -x
 
 #Author : Akash Suchak
-#Solving Using Switch Case Statement
+#Calculating wages for Month
 
 #Random Number Generate
 check=$((RANDOM%3))
@@ -10,21 +10,24 @@ check=$((RANDOM%3))
 wagePerHr=20
 fullDayHrs=8
 partDayHrs=4
+workingDays=20
 
-#Calculate Daily Wage
-dailyWagePartTime=$((wagePerHr * partDayHrs))
-dailyWageFullTime=$((wagePerHr * fullDayHrs))
-
-#Check Employee is FullTime, PArtTime or Absent
+#Check Employee is FullTime, PartTime or Absent
 #Display Wage accordingly
 case $check in
 	1)
+		dailyWageFullTime=$((wagePerHr * fullDayHrs))
+		monthlyWageFullTime=$((dailyWageFullTime * workingDays))
 		echo "Employee is Full Time Present"
 		echo "Daily Wage : $dailyWageFullTime"
+		echo "Monthly Wage : $monthlyWageFullTime"
 		;;
 	2)
+		dailyWagePartTime=$((wagePerHr * partDayHrs))
+		monthlyWagePartTime=$((dailyWagePartTime * workingDays))
 		echo "Employee is Part Time Present"
 		echo "Daily Wage : $dailyWagePartTime"
+		echo "Monthly Wage : $monthlyWagePartTime"
 		;;
 	*)
 		echo "Employee is Absent"
