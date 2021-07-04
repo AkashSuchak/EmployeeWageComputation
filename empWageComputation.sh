f6ce2bcb3411143b16401dfd5d982647777e1c06
AkashSuchak <suchak.akash@gmail.com>
2021-07-04 16:56:11 +0530

    Refactor : Code with Function

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,42 +1,54 @@
-#! /bin/bash 
+#! /bin/bash
 
 #Author : Akash Suchak
-#Calculate Wages till toal working hours or days is reached for a month
+#Refactor the code to write a function to get work hours
 
 #Assign values
-wagePerHr=20
-fullDayHrs=8
-partDayHrs=4
-workingDays=20
-totalWorkingHrs=0
+	wagePerHr=20
+	fullDayHrs=8
+	partDayHrs=4
+	#workingDays=20
+	totalWorkingHrs=0
 
-#Check Employee is FullTime, PartTime or Absent
-for (( days=1; days<=$workingDays; days++ ))
-do
-	#Random Number Generate
-	check=$((RANDOM%3))
+function workHours(){
+	workingDays=$1
 
-	case $check in
-		1)
-			#dailyWageFullTime=$((wagePerHr * fullDayHrs))
-			#monthlyWageFullTime=$((dailyWageFullTime * workingDays))
-			totalWorkingHrs=$((totalWorkingHrs + fullDayHrs))
-			;;
-		2)
-			#dailyWagePartTime=$((wagePerHr * partDayHrs))
-			#monthlyWagePartTime=$((dailyWagePartTime * workingDays))
-			totalWorkingHrs=$((totalWorkingHrs + partDaysHrs))
-			;;
-		*)
-			totalWorkingHrs=$((totalWorkingHrs + 0))
-			;;
-	esac
-
-	if [ $totalWorkingDays > 100 ]
+	if [ $workingDays -gt 0 ]
 	then
-		totalWorkingHrs=100
+		#Random Number Generate
+		check=$((RANDOM%3))
+
+		case $check in
+			1)
+				totalWorkingHrs=$(($totalWorkingHrs + $fullDayHrs))
+				;;
+			2)
+				totalWorkingHrs=$(($totalWorkingHrs + $partDayHrs))
+				;;
+			*)
+				totalWorkingHrs=$(($totalWorkingHrs + 0))
+				;;
+		esac
+
+		if [ $totalWorkingHrs -ge 100 ]
+		then
+			totalWorkingHrs=100
+			echo "$totalWorkingHrs"
+			return
+		fi
+		workingDays=$(($1 - 1))
+		workHours $workingDays
+
+	else
+		echo "$totalWorkingHours"
 	fi
-done
+}
+
+#calling Function
+workHours 20
+
+#cound and Display Total Monthly wage
 totalMonthlyWage=$((totalWorkingHrs * wagePerHr))
 echo "Total Working Hours of Month : $totalWorkingHrs"
 echo "Total Wages of Month : $totalMonthlyWage"
+
